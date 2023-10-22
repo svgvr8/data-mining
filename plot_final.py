9c3e204253a68eccb698568950e86a7d27df5b51
Sagar <sagarxyadav@gmail.com>
2023-10-21 22:20:10 -0400

    final plot

--- plot_final.py
+++ plot_final.py
@@ -45,8 +45,9 @@ def compute_frequent_itemsets(sparse_matrix, unique_items, min_support):
 min_support_counts = [50, 100, 150, 200]
 min_confs = [0.8, 0.95]
 
-# Initialize a dictionary to store the results
+# Initialize dictionaries to store the results
 runtimes = {conf: {"itemsets": [], "rules": []} for conf in min_confs}
+counts = {conf: {"itemsets": [], "rules": []} for conf in min_confs}
 
 for min_conf in min_confs:
     for min_support_count in min_support_counts:
@@ -58,8 +59,6 @@ for min_conf in min_confs:
             sparse_matrix, unique_items, min_support
         )
         new_itemsets = frequent_itemsets
-
-        # Check bigger combinations (like pairs, triplets of items, etc.) to see which sets are popular
         level = 1
         while new_itemsets:
             curr_level_itemsets = []
@@ -80,7 +79,10 @@ for min_conf in min_confs:
         end_time = time.time()
         runtimes[min_conf]["itemsets"].append(end_time - start_time)
 
-        # Measure runtime for rule creation
+        counts[min_conf]["itemsets"].append(len(frequent_itemsets))
+
+        # Measure runtime for rule creation and count the rules
+        rule_count = 0
         start_time = time.time()
         output_file_name = (
             f"output_file_rules_team5_conf_{min_conf}_sup_{min_support_count}.txt"
@@ -103,15 +105,15 @@ for min_conf in min_confs:
                             file.write(
                                 f"{' '.join(map(str, antecedent))}|{' '.join(map(str, consequent))}|{support:.2f}|{confidence:.2f}\n"
                             )
+                            rule_count += 1
         end_time = time.time()
         runtimes[min_conf]["rules"].append(end_time - start_time)
+        counts[min_conf]["rules"].append(rule_count)
 
-# Setting global font size for plots
-matplotlib.rcParams.update({"font.size": 14})
-
-# Now, plot and save the results
+# Now, plot and save the results for both runtimes and counts
 for min_conf in min_confs:
-    plt.figure(figsize=(12, 7))  # Increase the size of the figure
+    # Plotting runtimes
+    plt.figure(figsize=(12, 7))
     bars1 = plt.bar(
         np.array(range(len(min_support_counts))) - 0.2,
         runtimes[min_conf]["itemsets"],
@@ -128,8 +130,6 @@ for min_conf in min_confs:
         color="green",
         edgecolor="black",
     )
-
-    # Add value labels on top of the bars
     for bar in bars1:
         yval = bar.get_height()
         plt.text(
@@ -148,17 +148,62 @@ for min_conf in min_confs:
             ha="center",
             va="bottom",
         )
-
     plt.xticks(range(len(min_support_counts)), min_support_counts)
     plt.xlabel("Minimum Support Count", fontsize=16)
     plt.ylabel("Time (seconds)", fontsize=16)
     plt.title(f"Runtime for min_conf = {min_conf}", fontsize=18)
     plt.legend(loc="upper left", fontsize=14)
-
-    # Save the plot to a file
     file_name = f"runtime_plot_min_conf_{min_conf}.png"
-    plt.tight_layout()  # Adjusts the plot layout for better appearance
+    plt.tight_layout()
     plt.savefig(file_name)
-    print(f"Plot saved as {file_name}")
-
+    print(f"Runtime plot saved as {file_name}")
     plt.close()
+
+    # Plotting counts
+    plt.figure(figsize=(12, 7))
+    bars1 = plt.bar(
+        np.array(range(len(min_support_counts))) - 0.2,
+        counts[min_conf]["itemsets"],
+        0.4,
+        label="Frequent Itemsets",
+        color="blue",
+        edgecolor="black",
+    )
+    bars2 = plt.bar(
+        np.array(range(len(min_support_counts))) + 0.2,
+        counts[min_conf]["rules"],
+        0.4,
+        label="Rules",
+        color="green",
+        edgecolor="black",
+    )
+    for bar in bars1:
+        yval = bar.get_height()
+        plt.text(
+            bar.get_x() + bar.get_width() / 2,
+            yval + 0.05,
+            round(yval, 2),
+            ha="center",
+            va="bottom",
+        )
+    for bar in bars2:
+        yval = bar.get_height()
+        plt.text(
+            bar.get_x() + bar.get_width() / 2,
+            yval + 0.05,
+            round(yval, 2),
+            ha="center",
+            va="bottom",
+        )
+    plt.xticks(range(len(min_support_counts)), min_support_counts)
+    plt.xlabel("Minimum Support Count", fontsize=16)
+    plt.ylabel("Counts", fontsize=16)
+    plt.title(f"Counts for min_conf = {min_conf}", fontsize=18)
+    plt.legend(loc="upper left", fontsize=14)
+    file_name = f"counts_plot_min_conf_{min_conf}.png"
+    plt.tight_layout()
+    plt.savefig(file_name)
+    print(f"Counts plot saved as {file_name}")
+    plt.close()
+
+print("Processing completed!")
